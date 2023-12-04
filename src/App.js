dba54b063ae09b2a4f32cd45698822d6ea6e15bb
konurbaevbaiel <konurbaevbajel@gmail.com>
2023-12-04 16:21:21 +0600

    add: First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Game from "./components/Game/Game";
+import Result from "./components/Result/Result";
+import './style/style.scss'
+
 
 function App() {
+
+  const questions = [
+    {
+      title: 'Какая форма общего уравнения квадратичной функции?',
+      variants: [
+          'y = mx + b',
+          'y = ax^2 + bx + c',
+          'y = a/x',
+          'y = sqrt(x)'
+      ],
+      correct: 1
+    },
+    {
+      title: 'Как называется точка, в которой график квадратичной функции пересекает ось y?',
+      variants: [
+          'Вершина',
+          'Фокус',
+          'Асимптота',
+          'Пересечение'
+      ],
+      correct: 3
+    },
+    {
+      title: 'Как определить, ветви графика квадратичной функции направлены вверх или вниз?',
+      variants: [
+          'По коэффициенту a',
+          'По коэффициенту b',
+          'По коэффициенту c',
+          'По коэффициенту m'
+      ],
+      correct: 0
+    },
+    {
+      title: 'Как найти вершину графика квадратичной функции, заданной уравнением y = ax^2 + bx + c?',
+      variants: [
+          '(-b/2a, c)',
+          '(a/2, b/2)',
+          '(b/2a, c)',
+          '(-a/2, -b/2)'
+      ],
+      correct: 0
+    },
+    {
+      title: 'Как называется число, которое определяет положение вершины квадратичной функции на горизонтальной оси?',
+      variants: [
+          'Координата y',
+          'Координата x',
+          'Дискриминант',
+          'Коэффициент a'
+      ],
+      correct: 1
+    },
+    {
+      title: 'Что представляет собой дискриминант в квадратичном уравнении ax^2 + bx + c = 0?',
+      variants: [
+          'Коэффициент при x',
+          'Коэффициент при x^2',
+          'Корень уравнения',
+          'Выражение b^2 - 4ac'
+      ],
+      correct: 3
+    },
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Game/>
+        {/*<Result/>*/}
     </div>
   );
 }
---
+++ src/components/Game/Game.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Game = () => {
+    return (
+        <div className='game'>
+            <h2 className="game__title">Title</h2>
+
+            <ul className="game__list">
+                <li className="game__item">1</li>
+                <li className="game__item">2</li>
+                <li className="game__item">3</li>
+                <li className="game__item">4</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Game;
